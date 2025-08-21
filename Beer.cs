c2fc8ee2df71b82b29a1bde8869cf2bde7c51a87
pxxlpxxi <vaikeflyger@hotmail.com>
2025-08-21 10:34:09 +0200

    Add project files.

---
+++ Beer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerKlasse
+{
+    enum BeerType
+    {
+        Lager,
+        Pilsner,
+        Münchener,
+        WienerDortmunder,
+        Bock,
+        Doppelbock,
+        Porter,
+        Mix
+    }
+    internal class Beer : IComparable<Beer>
+    {
+        //private fields
+        private string _navn;
+        private BeerType _slags;
+        private float _procent;
+        private int _volumenCl;
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+        //properties, get og set til alle fields
+        public string Navn { get { return _navn; } set { _navn = value; } }
+        public BeerType Slags { get { return _slags; } set { _slags = value; } }
+        public float Procent { get { return _procent; } set { _procent = value; } }
+        public int VolumenCl { get { return _volumenCl; } set { _volumenCl = value; } }
+
+        public Beer()
+        {
+
+        }
+        public Beer(string navn, BeerType slags, float procent, int volumen)
+        {
+            _navn = navn;
+            _slags = slags;
+            _procent = procent;
+            _volumenCl = volumen;
+        }
+        public override string ToString()
+        {
+            string retur = $"\nNavn: {Navn}" +
+                $"\nType: {Slags}" +
+                $"\nProcent: {Procent}%" +
+                $"\nVolumen: {VolumenCl}cl.";
+            return retur; //base.ToString();
+        }
+        public double GetUnits()
+        {
+            return _volumenCl*_procent/150;
+        }
+        public static Beer operator + (Beer beer1, Beer beer2)
+        {
+            float nyPct = (beer1._volumenCl * beer1._procent + beer2._volumenCl * beer2._procent)
+                    / (beer1._volumenCl + beer2._volumenCl);
+            BeerType nyType = BeerType.Mix;
+            int nyVolumen=beer1._volumenCl + beer2._volumenCl;
+
+
+            return new Beer($"{beer1._navn}-{beer2._navn}", nyType, nyPct, nyVolumen);
+        }
+
+        int IComparable<Beer>.CompareTo(Beer? other)
+        {
+            return this.Procent.CompareTo(other.Procent);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace BeerKlasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Beer beer1 = new("Et", BeerType.Lager, 5f, 50);
+            Beer beer2 = new("To", BeerType.Doppelbock, 12f, 40);
+            Beer beer3 = new("Tre", BeerType.Porter, 7f, 30);
+            Beer beer4 = new("Fire", BeerType.WienerDortmunder, 5.5f, 33);
+            Beer beer5 = new("Fem", BeerType.Pilsner, 5f, 25);
+
+            Beer beer6 = (beer1 + beer4);
+
+            List<Beer> beerList = new() { beer1, beer2, beer3, beer4, beer5, beer6 };
+            beerList.Sort((a, b) => b.Procent.CompareTo(a.Procent));
+            beerList.ForEach(beer =>
+            {
+                Console.WriteLine(beer.ToString() + "\n__________________________");
+            });
+
+
+            string obj = "beer1";
+            Console.WriteLine($"---- {obj} ToString ----");
+            Console.WriteLine(beer1.ToString());
+
+            obj = "beer6";
+            Console.WriteLine($"---- {obj} ToString ----");
+            Console.WriteLine(beer6.ToString());
+
+            obj = "beer6 GetUnits";
+            Console.WriteLine($"---- {obj} ToString ----");
+            Console.WriteLine(beer6.GetUnits().ToString());
+
+            //obj = "beer6 Procent CompareTo beer1 Procent";
+            //float comparison = beer6.Procent.CompareTo(beer1.Procent);
+            //Console.WriteLine($"---- {obj} ----");
+            //Console.WriteLine(comparison);
+
+
+        }
+    }
+}
